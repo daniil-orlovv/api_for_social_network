c575a3c526e5c747b9c82b17d615aff528db6d9e
Даниил Орлов <danilorlov9898@yandex.ru>
2023-04-23 03:22:22 +0300

    Fix

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from django.core.validators import UniqueTogetherValidator
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post, Follow, Group
+from posts.models import Comment, Post, Follow, Group, User
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -28,11 +28,21 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(
-        read_only=True, required=False
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
     )
-    following = serializers.SlugRelatedField(
-        queryset=Follow.objects.all(), slug_field='following', required=False)
+    following = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault(),)
+
+    def validate(self, data):
+        user = data.get('user')
+        following = data.get('following')
+        if user == following:
+            raise serializers.ValidationError(
+                'Пользователь не может подписаться на самого себя!')
+        return data
 
     class Meta:
         fields = ('user', 'following')
