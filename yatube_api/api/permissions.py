eeaa17cdd8dd4f5f35803dd3200ad035018e320a
Даниил Орлов <danilorlov9898@yandex.ru>
2023-04-25 20:09:59 +0300

    Added cheking safe methods

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,7 +4,13 @@ from rest_framework import permissions
 class AuthAuthorOnlyPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated
+        return (
+            request.user.is_authenticated
+            or request.method in permissions.SAFE_METHODS
+        )
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return (
+            obj.author == request.user
+            or request.method in permissions.SAFE_METHODS
+            )
